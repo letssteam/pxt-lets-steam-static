1b4ba1f8facd31f09f0a36ee0192ca7136cdad74
nedseb <sebastien@nedjar.com>
2022-02-28 10:55:49 +0000

    Update from https://github.com/letssteam/pxt-lets-steam/commit/2a8f40ff48264c1c2b74827717a4dc66a966e48e

--- sim.js
+++ sim.js
@@ -2081,6 +2081,14 @@ var pxsim;
         }
     }
     pxsim.ThermometerState = ThermometerState;
+    function setTemperatureUnit(unit) {
+        pxsim.board().thermometerUnitState = unit;
+    }
+    pxsim.setTemperatureUnit = setTemperatureUnit;
+    function temperatureUnit() {
+        return pxsim.board().thermometerUnitState;
+    }
+    pxsim.temperatureUnit = temperatureUnit;
 })(pxsim || (pxsim = {}));
 /// <reference path="../../../node_modules/pxt-core/built/pxtsim.d.ts"/>
 /// <reference path="../../../built/common-sim.d.ts"/>
@@ -2092,6 +2100,15 @@ var pxsim;
         class ThermometerView {
             constructor() {
                 this.style = visuals.BUTTON_PAIR_STYLE;
+                this.isOpen = false;
+                this.INPUT_ID = "TEMPERATURE-RANGE";
+                this.BOARD_ICON_ID = `BUTTON-${this.INPUT_ID}`;
+                this.ICON_SVG = `<rect x="0" y="0" width="237.9964" height="498.67801" fill="#00000000" /><g id="g148" transform="translate(-231.003,-30.6692)"> <path d="m 359.8,362.63 v -85.289 h -22.398 v 85.289 c -22.438,5.0977 -39.199,25.129 -39.199,49.113 0,27.836 22.562,50.398 50.398,50.398 27.836,0 50.398,-22.562 50.398,-50.398 0,-23.988 -16.762,-44.02 -39.199,-49.113 z" id="path70" /><path d="M 460.6,218.54 H 421.944 V 179.341 H 460.6 c 4.6406,0 8.3984,-3.7617 8.3984,-8.3984 0,-4.6367 -3.7578,-8.3984 -8.3984,-8.3984 h -38.656 v -58.801 c 0,-40.359 -32.711,-73.074 -73.07,-73.074 -40.359,0 -73.07,32.715 -73.07,73.074 v 215.75 c -27.254,21.539 -44.801,54.812 -44.801,92.254 0,64.961 52.641,117.6 117.6,117.6 64.961,0 117.6,-52.641 117.6,-117.6 0,-37.199 -17.309,-70.297 -44.258,-91.844 v -84.559 h 38.656 c 4.6406,0 8.3984,-3.7617 8.3984,-8.3984 0,-4.6406 -3.7617,-8.4023 -8.4023,-8.4023 z m -49.152,114.47 c 24.121,19.285 37.953,47.98 37.953,78.73 0,55.578 -45.219,100.8 -100.8,100.8 -55.578,0 -100.8,-45.219 -100.8,-100.8 0,-30.957 14,-59.781 38.414,-79.07 l 6.3828,-5.0391 v -8.1367 l 0.004,-215.76 c 0,-31.031 25.246,-56.273 56.27,-56.273 31.023,0 56.27,25.246 56.27,56.273 v 58.801 h -50.941 c -4.6406,0 -8.3984,3.7617 -8.3984,8.3984 0,4.6367 3.7578,8.3984 8.3984,8.3984 h 50.941 v 39.199 l -50.941,0.004 c -4.6406,0 -8.3984,3.7617 -8.3984,8.3984 0,4.6367 3.7578,8.3984 8.3984,8.3984 h 50.941 v 92.629 z" id="path72" /></g>`;
+                this.BACKGROUND_COLOR = "#39474e";
+                // Celsius
+                this.tmin = -5;
+                this.tmax = 50;
+                this.unitPerKeyPress = 1;
             }
             init(bus, state, svgEl, otherParams) {
                 this.state = state;
@@ -2099,129 +2116,150 @@ var pxsim;
                 this.defs = [];
                 this.svgEl = svgEl;
                 this.updateState();
-                this.attachEvents();
             }
             moveToCoord(xy) {
-                let btnWidth = visuals.PIN_DIST * 3;
-                let [x, y] = xy;
-                visuals.translateEl(this.btn, [x, y]);
             }
             updateState() {
                 let state = this.state;
                 if (!state || !state.thermometerState || !state.thermometerState.sensorUsed) {
-                    if (this.thermometer) {
-                        this.svgEl.removeChild(this.element);
-                        this.thermometer = null;
+                    if (this.sliderDiv) {
+                        this.svgEl.removeChild(this.board_icon);
+                        this.svgEl.removeChild(this.text);
+                        document.body.removeChild(this.sliderDiv);
+                        this.sliderDiv = null;
                     }
                 }
                 else if (state && state.thermometerState && state.thermometerState.sensorUsed) {
-                    this.mkThermometer();
-                    this.svgEl.appendChild(this.element);
-                    this.updateTemperature();
+                    if (!this.sliderDiv) {
+                        this.mkThermometer();
+                        this.svgEl.appendChild(this.board_icon);
+                        this.svgEl.appendChild(this.text);
+                        document.body.appendChild(this.sliderDiv);
+                        this.updateTemperature();
+                        this.board_icon.dispatchEvent(new Event("click"));
+                    }
                 }
             }
             getElement() {
                 return this.element;
             }
             updateTheme() {
-                let On = "#ff7777";
-                let Off = "#fff";
-                pxsim.svg.setGradientColors(this.thermometerGradient, Off, On);
             }
             mkThermometer() {
-                let svgEl = this.svgEl;
-                let defs = pxsim.svg.child(svgEl, "defs", {});
-                let g = pxsim.svg.elt("g");
-                if (!this.thermometer) {
-                    let gid = "gradient-thermometer";
-                    this.thermometerGradient = pxsim.svg.linearGradient(defs, gid);
-                    let xBase = 25;
-                    let yBase = 15;
-                    let heightBase = 64;
-                    pxsim.svg.child(g, "rect", {
-                        fill: "transparent",
-                        x: xBase - 5,
-                        y: yBase - 20,
-                        width: 20,
-                        height: heightBase + 40,
-                    });
-                    this.thermometer = pxsim.svg.child(g, "rect", {
-                        class: "sim-thermometer no-drag",
-                        x: xBase,
-                        y: yBase,
-                        width: 10,
-                        height: heightBase,
-                        rx: 4, ry: 4,
-                        fill: `url(#${gid})`
-                    });
-                    this.thermometerText = pxsim.svg.child(g, "text", { class: 'sim-text', x: xBase + 20, y: yBase + 20 });
-                    this.updateTheme();
-                    let pt = svgEl.createSVGPoint();
-                    pxsim.svg.buttonEvents(g, 
-                    // move
-                    (ev) => {
-                        let cur = pxsim.svg.cursorPoint(pt, svgEl, ev);
-                        let t = Math.max(0, Math.min(1, ((heightBase + yBase) - cur.y) / (heightBase)));
-                        this.state.thermometerState.setLevel(Math.floor(ThermometerView.tmin + t * (ThermometerView.tmax - ThermometerView.tmin)));
-                        this.updateTemperature();
-                    }, 
-                    // start
-                    ev => { }, 
-                    // stop
-                    ev => { }, 
-                    // keydown
-                    (ev) => {
-                        let charCode = (typeof ev.which == "number") ? ev.which : ev.keyCode;
-                        if (charCode === 40 || charCode === 37) { // Down/Left arrow
-                            if (this.state.thermometerState.getLevel() === -5) {
-                                this.state.thermometerState.setLevel(50);
-                            }
-                            else {
-                                this.state.thermometerState.setLevel(this.state.thermometerState.getLevel() - 1);
-                            }
-                            this.updateTemperature();
-                        }
-                        else if (charCode === 38 || charCode === 39) { // Up/Right arrow
-                            if (this.state.thermometerState.getLevel() === 50) {
-                                this.state.thermometerState.setLevel(-5);
-                            }
-                            else {
-                                this.state.thermometerState.setLevel(this.state.thermometerState.getLevel() + 1);
-                            }
-                            this.updateTemperature();
-                        }
-                    });
-                    pxsim.accessibility.makeFocusable(this.thermometer);
-                    pxsim.accessibility.setAria(this.thermometer, "slider", "Thermometer");
-                    this.thermometer.setAttribute("aria-valuemin", ThermometerView.tmin.toString());
-                    this.thermometer.setAttribute("aria-valuemax", ThermometerView.tmax.toString());
-                    this.thermometer.setAttribute("aria-orientation", "vertical");
+                if (this.sliderDiv) {
+                    return;
                 }
-                this.element = g;
-            }
-            attachEvents() {
+                this.sliderDiv = document.createElement("div");
+                let icon = document.createElement("div");
+                this.slider = document.createElement("input");
+                this.board_icon = pxsim.svg.elt("g");
+                this.text = pxsim.svg.elt("text", { x: 75, y: 30, "font-family": "monospace", "font-size": 25, fill: "#FFFFFF" });
+                this.board_icon.id = this.BOARD_ICON_ID;
+                this.board_icon.style.cursor = "pointer";
+                this.board_icon.innerHTML = this.generateIcon(65, 65, 25, 15);
+                this.board_icon.onclick = () => {
+                    this.sliderDiv.style.display = "block";
+                    setTimeout(() => { this.isOpen = true; }, 250); // Avoid immediate closing
+                };
+                document.addEventListener("click", (ev) => {
+                    if (!this.isOpen) {
+                        return;
+                    }
+                    for (let i = 0; i < ev.path.length; ++i) {
+                        if (ev.path[i].id == this.INPUT_ID || ev.path[i].id == this.BOARD_ICON_ID) {
+                            return;
+                        }
+                    }
+                    this.sliderDiv.style.display = "none";
+                    this.isOpen = false;
+                });
+                document.addEventListener("keydown", (ev) => {
+                    if (!this.isOpen) {
+                        return;
+                    }
+                    switch (ev.key) {
+                        case "ArrowUp":
+                            this.slider.valueAsNumber += this.unitPerKeyPress;
+                            break;
+                        case "ArrowDown":
+                            this.slider.valueAsNumber -= this.unitPerKeyPress;
+                            break;
+                    }
+                });
+                this.sliderDiv.style.position = "absolute";
+                this.sliderDiv.style.top = "0";
+                this.sliderDiv.style.left = "0";
+                this.sliderDiv.style.width = "100%";
+                this.sliderDiv.style.height = "15px";
+                this.sliderDiv.style.transform = "translate(-50%) rotate(270deg) translate(-50%, 50%)";
+                this.sliderDiv.style.display = "none";
+                this.sliderDiv.style.backgroundColor = this.BACKGROUND_COLOR;
+                icon.style.width = "12px";
+                icon.style.position = "absolute";
+                icon.style.top = "50%";
+                icon.style.right = "2px";
+                icon.style.transform = "translate(0, -50%) rotate(90deg)";
+                icon.style.backgroundColor = this.BACKGROUND_COLOR;
+                icon.innerHTML = this.generateIcon();
+                this.slider.id = this.INPUT_ID;
+                this.slider.type = "range";
+                this.slider.min = this.tmin.toString();
+                this.slider.max = this.tmax.toString();
+                this.slider.value = this.state.thermometerState.getLevel().toString();
+                this.slider.style.width = "calc(100% - 20px - 15px)";
+                this.slider.style.display = "inline-block";
+                this.slider.style.position = "absolute";
+                this.slider.style.left = "15px";
+                this.slider.style.top = "50%";
+                this.slider.style.margin = "0";
+                this.slider.style.transform = "translate(0, -50%)";
+                this.slider.style.setProperty("appearance", "none");
+                this.slider.style.height = "5px";
+                this.slider.style.borderRadius = "100px";
+                this.slider.style.background = "linear-gradient(90deg, rgb(73, 195, 243) 0%, rgb(255 79 79) 100%)";
+                this.slider.oninput = (ev) => {
+                    this.state.thermometerState.setLevel(parseInt(this.slider.value));
+                    this.updateTemperature();
+                };
+                this.sliderDiv.append(icon);
+                this.sliderDiv.append(this.slider);
+                this.sliderDiv.append(this.text);
             }
             updateTemperature() {
                 let state = this.state;
                 if (!state || !state.thermometerState || !state.thermometerState.sensorUsed)
                     return;
-                let t = Math.max(ThermometerView.tmin, Math.min(ThermometerView.tmax, state.thermometerState.getLevel()));
-                let per = Math.floor((state.thermometerState.getLevel() - ThermometerView.tmin) / (ThermometerView.tmax - ThermometerView.tmin) * 100);
-                pxsim.svg.setGradientValue(this.thermometerGradient, 100 - per + "%");
-                let unit = "°C";
-                if (state.thermometerUnitState == pxsim.TemperatureUnit.Fahrenheit) {
-                    unit = "°F";
-                    t = ((t * 18) / 10 + 32) >> 0;
+                let t = Math.max(this.tmin, Math.min(this.tmax, state.thermometerState.getLevel()));
+                let unit = "";
+                switch (pxsim.temperatureUnit()) {
+                    case pxsim.TemperatureUnit.Celsius:
+                        unit = "°C";
+                        break;
+                    case pxsim.TemperatureUnit.Fahrenheit:
+                        unit = "°F";
+                        t = ((t * 18) / 10 + 32);
+                        break;
                 }
-                this.thermometerText.textContent = t + unit;
-                this.thermometer.setAttribute("aria-valuenow", t.toString());
-                this.thermometer.setAttribute("aria-valuetext", t + unit);
+                this.text.textContent = `${t + unit}`;
                 pxsim.accessibility.setLiveContent(t + unit);
             }
+            generateIcon(width, height, x, y) {
+                let svgTag = `<svg version="1.1" viewBox="0 0 237.9964 498.67801" id="svg150" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" fill="#FFFFFF"`;
+                if (width != undefined && width > 0) {
+                    svgTag += ` ${svgTag} width="${width}" `;
+                }
+                if (height != undefined && height > 0) {
+                    svgTag += ` ${svgTag} height="${height}"`;
+                }
+                if (x != undefined && x > 0) {
+                    svgTag += ` ${svgTag} x="${x}"`;
+                }
+                if (y != undefined && y > 0) {
+                    svgTag += ` ${svgTag} y="${y}"`;
+                }
+                return `${svgTag}>${this.ICON_SVG}</svg>`;
+            }
         }
-        // Celsius
-        ThermometerView.tmin = -5;
-        ThermometerView.tmax = 50;
         visuals.ThermometerView = ThermometerView;
     })(visuals = pxsim.visuals || (pxsim.visuals = {}));
 })(pxsim || (pxsim = {}));
