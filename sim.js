cae7fc1a324f870e393cf37f84e9fdfbaab279ac
nedseb <sebastien@nedjar.com>
2023-09-11 15:49:07 +0000

    Update from https://github.com/letssteam/pxt-lets-steam/commit/4f0f2d2702686a27316f9475f88843542f0b19cc

--- sim.js
+++ sim.js
@@ -84,6 +84,7 @@ var pxsim;
             this.soilHygrometerState = new pxsim.SoilHygrometerState();
             this.sen0422State = new pxsim.ECMeterSEN0244State();
             this.anemometerState = new pxsim.AnemometerState();
+            this.dbMeterState = new pxsim.AnalogSensorState(22 /* DEVICE_ID_SYSTEM_LEVEL_DETECTOR_SPL */, 0, 120);
             // TODO we need this.buttonState set for pxtcore.getButtonByPin(), but
             // this should be probably merged with buttonpair somehow
             this.builtinParts["radio"] = this.radioState = new pxsim.RadioState(pxsim.runtime, this, {
@@ -170,6 +171,8 @@ var pxsim;
             this.builtinParts["anemometer"] = this.anemometerState;
             this.builtinVisuals["anemometer"] = () => new pxsim.visuals.AnemometerView();
             this.builtinPartVisuals["anemometer"] = (xy) => pxsim.visuals.mkAnemometer(xy);
+            this.builtinParts["dbmeter"] = new pxsim.DBMeterState(this.dbMeterState);
+            this.builtinVisuals["dbmeter"] = () => new pxsim.visuals.DBMeterView();
         }
         kill() {
             super.kill();
@@ -496,6 +499,7 @@ var pxsim;
                 this.onBoardThermometer = new visuals.ThermometerView();
                 this.onBoardBarometer = new visuals.BarometerView();
                 this.onBoardCompass = new visuals.CompassView();
+                this.onBoardDBMeter = new visuals.DBMeterView();
                 if (props && props.theme)
                     this.updateTheme();
                 if (props && props.runtime) {
@@ -508,6 +512,7 @@ var pxsim;
                 this.onBoardThermometer.init(this.board.bus, new pxsim.ThermometerState(this.board.thermometerState), el, null);
                 this.onBoardBarometer.init(this.board.bus, new pxsim.BarometerState(this.board.barometerState, this.board.pressureUnitState), el, null);
                 this.onBoardCompass.init(this.board.bus, this.board.compassState, el, null);
+                this.onBoardDBMeter.init(this.board.bus, new pxsim.DBMeterState(this.board.dbMeterState), el, null);
             }
             updateTheme() {
                 let theme = this.props.theme;
@@ -531,6 +536,7 @@ var pxsim;
                 this.onBoardThermometer.updateState();
                 this.onBoardBarometer.updateState();
                 this.onBoardCompass.updateState();
+                this.onBoardDBMeter.updateState();
                 this.updateGestures();
             }
             updateGestures() {
@@ -1483,6 +1489,189 @@ var pxsim;
             }
         }
         visuals.BarometerView = BarometerView;
+    })(visuals = pxsim.visuals || (pxsim.visuals = {}));
+})(pxsim || (pxsim = {}));
+var pxsim;
+(function (pxsim) {
+    var input;
+    (function (input) {
+        function ondbMeterConditionChanged(condition, value, body) {
+            let b = pxsim.dbMeterState();
+            b.setUsed();
+            if (condition == 2 /* ANALOG_THRESHOLD_HIGH */) {
+                b.setHighThreshold(value);
+            }
+            else {
+                b.setLowThreshold(value);
+            }
+            pxsim.pxtcore.registerWithDal(b.id, condition, body);
+        }
+        input.ondbMeterConditionChanged = ondbMeterConditionChanged;
+        function decibel() {
+            let b = pxsim.dbMeterState();
+            b.setUsed();
+            return b.getLevel();
+        }
+        input.decibel = decibel;
+    })(input = pxsim.input || (pxsim.input = {}));
+})(pxsim || (pxsim = {}));
+/// <reference path="../../../node_modules/pxt-core/built/pxtsim.d.ts"/>
+/// <reference path="../../../built/common-sim.d.ts"/>
+/// <reference path="../../../built/common-sim.d.ts"/>
+/// <reference path="../../../node_modules/pxt-core/built/pxtsim.d.ts"/>
+var pxsim;
+(function (pxsim) {
+    class DBMeterState {
+        constructor(dbMeterState) {
+            this.dbMeterState = dbMeterState;
+        }
+    }
+    pxsim.DBMeterState = DBMeterState;
+    function dbMeterState() {
+        return pxsim.board().dbMeterState;
+    }
+    pxsim.dbMeterState = dbMeterState;
+})(pxsim || (pxsim = {}));
+/// <reference path="../../../node_modules/pxt-core/built/pxtsim.d.ts"/>
+/// <reference path="../../../built/common-sim.d.ts"/>
+/// <reference path="../../../libs/core/dal.d.ts"/>
+var pxsim;
+(function (pxsim) {
+    var visuals;
+    (function (visuals) {
+        class DBMeterView {
+            constructor() {
+                this.style = visuals.BUTTON_PAIR_STYLE;
+                this.isOpen = false;
+                this.INPUT_ID = "DBMETER-RANGE";
+                this.ICON_SVG = `<path d="M33 46V22c0-9.35 7.65-17 17-17s17 7.65 17 17v24c0 9.35-7.65 17-17 17s-17-7.65-17-17zm46 0H69c0 10.477-8.523 19-19 19s-19-8.523-19-19H21c0 14.284 10.386 26.183 24 28.56V85H34c-1.65 0-3 1.35-3 3v4c0 1.65 1.35 3 3 3h32c1.65 0 3-1.35 3-3v-4c0-1.65-1.35-3-3-3H55V74.56C68.614 72.183 79 60.284 79 46z"/>`;
+                this.dBmin = 0;
+                this.dBmax = 120;
+            }
+            init(bus, state, svgEl, otherParams) {
+                this.state = state;
+                this.bus = bus;
+                this.defs = [];
+                this.svgEl = svgEl;
+                this.updateState();
+            }
+            moveToCoord(xy) {
+            }
+            updateState() {
+                let state = this.state;
+                if (!state || !state.dbMeterState || !state.dbMeterState.sensorUsed) {
+                    if (this.sliderDiv) {
+                        this.svgEl.removeChild(this.board_icon);
+                        this.svgEl.removeChild(this.text);
+                        document.body.removeChild(this.sliderDiv);
+                        this.sliderDiv = null;
+                    }
+                }
+                else if (state && state.dbMeterState && state.dbMeterState.sensorUsed) {
+                    if (!this.sliderDiv) {
+                        this.mkDBMeter();
+                        this.svgEl.appendChild(this.board_icon);
+                        this.svgEl.appendChild(this.text);
+                        document.body.appendChild(this.sliderDiv);
+                        this.updateDecibel();
+                        this.board_icon.dispatchEvent(new Event("click"));
+                    }
+                }
+            }
+            getElement() {
+                return this.element;
+            }
+            updateTheme() {
+            }
+            mkDBMeter() {
+                if (this.sliderDiv) {
+                    return;
+                }
+                this.sliderDiv = document.createElement("div");
+                let icon = document.createElement("div");
+                this.slider = document.createElement("input");
+                this.board_icon = pxsim.svg.elt("g");
+                this.text = pxsim.svg.elt("text", { x: 233, y: 520, "font-family": "monospace", "font-size": 25, fill: "#FFFFFF" });
+                this.board_icon.style.cursor = "pointer";
+                this.board_icon.innerHTML = this.generateIcon(60, 60, 235, 440);
+                this.board_icon.onclick = () => {
+                    this.sliderDiv.style.display = "block";
+                    pxsim.SimGaugeMessage.askClose(this.INPUT_ID);
+                    this.isOpen = true;
+                };
+                this.sliderDiv.style.position = "absolute";
+                this.sliderDiv.style.top = "0";
+                this.sliderDiv.style.left = "0";
+                this.sliderDiv.style.width = "100%";
+                this.sliderDiv.style.height = "15px";
+                this.sliderDiv.style.transform = "translate(-50%) rotate(270deg) translate(-50%, 50%)";
+                this.sliderDiv.style.display = "none";
+                icon.style.width = "15px";
+                icon.style.position = "absolute";
+                icon.style.top = "50%";
+                icon.style.right = "0";
+                icon.style.transform = "translate(0, -50%) rotate(90deg)";
+                icon.innerHTML = this.generateIcon();
+                this.slider.id = this.INPUT_ID;
+                this.slider.type = "range";
+                this.slider.min = this.dBmin.toString();
+                this.slider.max = this.dBmax.toString();
+                this.slider.value = this.state.dbMeterState.getLevel().toString();
+                this.slider.style.width = "calc(100% - 20px - 15px)";
+                this.slider.style.display = "inline-block";
+                this.slider.style.position = "absolute";
+                this.slider.style.left = "15px";
+                this.slider.style.top = "50%";
+                this.slider.style.margin = "0";
+                this.slider.style.transform = "translate(0, -50%)";
+                this.slider.style.setProperty("appearance", "none");
+                this.slider.style.height = "5px";
+                this.slider.style.borderRadius = "100px";
+                this.slider.style.background = "linear-gradient(90deg, rgb(0 164 255) 0%, rgb(255 0 0) 100%)";
+                this.slider.oninput = (ev) => {
+                    this.state.dbMeterState.setLevel(parseInt(this.slider.value));
+                    this.updateDecibel();
+                };
+                this.sliderDiv.append(icon);
+                this.sliderDiv.append(this.slider);
+                this.sliderDiv.append(this.text);
+                pxsim.SimGaugeMessage.registerOnAskClose(this.INPUT_ID, (id) => {
+                    if (!this.isOpen) {
+                        return;
+                    }
+                    this.sliderDiv.style.display = "none";
+                    this.isOpen = false;
+                });
+            }
+            updateDecibel() {
+                let state = this.state;
+                if (!state || !state.dbMeterState || !state.dbMeterState.sensorUsed)
+                    return;
+                let t = Math.max(this.dBmin, Math.min(this.dBmax, state.dbMeterState.getLevel()));
+                this.text.textContent = `${t}dB`;
+                pxsim.accessibility.setLiveContent(t + "dB");
+            }
+            generateIcon(width, height, x, y) {
+                let svgTag = `<svg version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" xml:space="preserve"`;
+                if (width != undefined && width > 0) {
+                    svgTag += ` ${svgTag} width="${width}" `;
+                }
+                if (height != undefined && height > 0) {
+                    svgTag += ` ${svgTag} height="${height}"`;
+                }
+                if (x != undefined && x > 0) {
+                    svgTag += ` ${svgTag} x="${x}"`;
+                }
+                if (y != undefined && y > 0) {
+                    svgTag += ` ${svgTag} y="${y}"`;
+                }
+                return `${svgTag}>${this.ICON_SVG}</svg>`;
+            }
+            constraintValue(value) {
+                return Math.min(this.dBmax, Math.max(this.dBmin, value));
+            }
+        }
+        visuals.DBMeterView = DBMeterView;
     })(visuals = pxsim.visuals || (pxsim.visuals = {}));
 })(pxsim || (pxsim = {}));
 var pxsim;
